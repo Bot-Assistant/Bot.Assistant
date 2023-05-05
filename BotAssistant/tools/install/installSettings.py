2ca83e7d6a169394e85cc8e25d46130b5fdfb2be
Ted <84779771+Ted-18@users.noreply.github.com>
2023-05-05 15:45:47 +0200

    v0.10.1-alpha

--- BotAssistant/tools/install/installSettings.py
+++ BotAssistant/tools/install/installSettings.py
@@ -1,12 +1,15 @@
+import os 
 import tools.install.messages.messageLogo as messageLogo
 import tools.install.settings.settingBotEditor as settingBotEditor
 import tools.install.settings.settingQuestions as settingQuestions
 
 def install():
 
-    messageLogo.send()
-    if input("Write 'y' to configure settings, space to skip:") != "y":
-        return
+    if os.path.exists("settings/settingDatabase.py"):
+        
+        messageLogo.send()
+        if input("Write 'y' to configure settings, space to skip:") != "y":
+            return
 
     settingBotEditor.settingBotBool("consoleColor", settingQuestions.colorSetting())
     settingBotEditor.settingBotBool("debug", settingQuestions.debugSetting())
