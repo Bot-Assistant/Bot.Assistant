d1107b94d823ff493a4559072f6fbc6daf098fd7
Ted <84779771+Ted-18@users.noreply.github.com>
2023-06-26 07:36:28 +0200

    Update serviceAddonManager.py

--- services/serviceAddonManager.py
+++ services/serviceAddonManager.py
@@ -1,29 +1,24 @@
-
 import importlib
 
-# Description: Get the permissions list of an addon (without discord permissions)
+# Description: Get the permissions list of an addon (excluding Discord permissions)
 # Return: List of permissions
 def getPermissionsList(addon):
-
     if addon is None:
         return []
 
-    # Import the init file of the addon
+    # Import the addon's init file
     addonInit = importlib.import_module("addons." + addon + ".init")
 
     # Get the permissions
-    commandPermissions = addonInit.commandPermissions
+    commandPermissions = getattr(addonInit, "commandPermissions", {})
 
     permissions = []
 
     for key, value in commandPermissions.items():
-        
-        # Check if the permission is a discord permission
+        # Check if the permission is a Discord permission
         if value.startswith("discord.permission."):
             continue
 
         permissions.append(value)
 
     return permissions
-
-
--- services/serviceAddonUpdater.py
+++ services/serviceAddonUpdater.py
@@ -1,16 +1,12 @@
 import os
 import importlib
-
+from services.serviceLogger import Logger
+import services.serviceConsoleMessages as serviceConsoleMessages
+import services.serviceGitHub as serviceGitHub
 import settings.settingBot as settingBot
 
-import services.serviceGitHub as serviceGitHub
-import services.serviceConsoleMessages as serviceConsoleMessages
-from services.serviceLogger import Logger
-
-
 def updateAddon():
-
-    if settingBot.addonUpdate == False:
+    if not settingBot.addonUpdate:
         Logger.system("Addon update is disabled")
         return
     
@@ -19,33 +15,23 @@ def updateAddon():
     for root, dirs, files in os.walk("addons"):
         for file in files:
             if file == "init.py":
-                
-                if os.name == 'nt':
-                    initFile = os.path.join(root, file).replace("\\", ".")[:-3]
-                elif os.name == 'posix':
-                    initFile = os.path.join(root, file).replace("/", ".")[:-3]
-                else:
-                    Logger.system("OS: Unknown")
-
+                initFile = os.path.join(root, file).replace(os.sep, ".")[:-3]
                 importedFile = importlib.import_module(initFile)
 
-                if importedFile.cogEnabled == False:
+                if not importedFile.cogEnabled:
                     continue
                 
-                if os.name == 'nt':
-                    addonName = root.replace("addons\\", "")
-                elif os.name == 'posix':
-                    addonName = root.replace("addons/", "")
-                    
-                if importedFile.enableGithub != True:
+                addonName = root.replace("addons" + os.sep, "")
+
+                if not importedFile.enableGithub:
                     continue
                 
                 repositoryVersion = serviceGitHub.getLatestRelease(importedFile.repository, importedFile.version, importedFile.author)
 
-                if repositoryVersion == None:
+                if repositoryVersion is None:
                     return
 
-                elif repositoryVersion == False:
+                elif repositoryVersion is False:
                     Logger.update(f"Addon: {addonName} - Up to date")
 
                 # Verify if the repository version is different from the local version
@@ -54,40 +40,24 @@ def updateAddon():
                     
                 elif repositoryVersion > importedFile.version:
                     Logger.update(f"Addon: {addonName} - Update available")
-
-                    # Print the logo
                     serviceConsoleMessages.logo()
-
                     Logger.update(f"Addon: {addonName} - Version: {importedFile.version} > {repositoryVersion}")
 
-                    # Ask if you want really to update the addon
+                    # Ask if you really want to update the addon
                     print(" ")
-                    print("Write 'y' to update the addon, everything else to cancel")
+                    print("Write 'y' to update the addon, anything else to cancel")
                     update = input("Answer: ")
 
-                    # If the answer is not "y" then cancel the update
+                    # If the answer is not "y", cancel the update
                     if update != "y":
                         return
 
                     # Update the addon
                     Logger.update(f"Addon: {addonName} - Updating...")
-
-                    # Download the latest release
                     serviceGitHub.updateLatestReleaseAddons(importedFile.author, importedFile.repository, addonName)
 
                     updatedAddon = True
 
-    if updatedAddon == True:
-        Logger.update(f"PLEASE RESTART THE BOT TO APPLY THE CHANGES")
+    if updatedAddon:
+        Logger.update("PLEASE RESTART THE BOT TO APPLY THE CHANGES")
         os._exit(0)
-
-
-
-
-
-            
-        
-                
-                
-
-                
